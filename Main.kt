ff7f0067bb13991f30f0b8349d41b57a034fe212
Mateusz Galaś <mateuszgalas94@gmail.com>
2023-02-08 20:33:21 +0100

    Changing a way of splitting elements, clean out.

--- Main.kt
+++ Main.kt
@@ -5,7 +5,7 @@ import kotlin.math.sqrt
 import kotlin.system.exitProcess
 
 class PhoneBook {
-    val book: MutableList<String>
+    private val book: MutableList<String>
     private val find: List<String>
     private var linearSearchDuration = 0L
 
@@ -18,7 +18,7 @@ class PhoneBook {
         find = findFile.readLines()
     }
 
-    fun findNumbers() {
+    fun linearSearch() {
         var counter = 0
 
         val start = System.currentTimeMillis()
@@ -49,10 +49,8 @@ class PhoneBook {
             sorted = true
 
             for (i in 0 until book.lastIndex) {
-                val value = book[i].split(" ")
-                val nextValue = book[i + 1].split(" ")
-                val element = if (value.size > 2) "${value[1]} ${value[2]}" else value[1]
-                val nextElement = if (nextValue.size > 2) "${nextValue[1]} ${nextValue[2]}" else nextValue[1]
+                val element = book[i].substringAfter(" ")
+                val nextElement = book[i + 1].substringAfter(" ")
 
                 if (element > nextElement) {
                     book[i] = book[i + 1].also { book[i + 1] = book[i] }
@@ -63,17 +61,20 @@ class PhoneBook {
             duration = end - start
             if (duration > linearSearchDuration * 10) {
                 linearSearchDuration = duration
-                findNumbers()
-                println("Sorting time: " +
-                        "${duration / 60000} min. ${(duration % 60000) / 1000} sec. ${duration % 1000} ms. " +
-                        "- STOPPED, moved to linear search")
+                linearSearch()
+                println(
+                    "Sorting time: " +
+                            "${duration / 60000} min. ${(duration % 60000) / 1000} sec. ${duration % 1000} ms. " +
+                            "- STOPPED, moved to linear search"
+                )
                 duration = linearSearchDuration - duration
-                        println("Searching time: " +
-                        "${duration / 60000} min. ${(duration % 60000) / 1000} sec. ${duration % 1000} ms. ")
+                println(
+                    "Searching time: " +
+                            "${duration / 60000} min. ${(duration % 60000) / 1000} sec. ${duration % 1000} ms. "
+                )
                 return
             }
         }
-        File("sorted.txt").writeText(book.joinToString("\n"))
 
         println(
             "Sorting time: " +
@@ -82,7 +83,7 @@ class PhoneBook {
         jumpSearch()
     }
 
-    fun jumpSearch() {
+    private fun jumpSearch() {
         val step = sqrt(book.size.toDouble()).toInt()
         var counter = 0
 
@@ -90,9 +91,8 @@ class PhoneBook {
         for (element in find) {
             var n = 0
 
-            loop@while (n <= book.lastIndex) {
-                val list = book[n].split(" ")
-                val elementInBook = if (list.size > 2) "${list[1]} ${list[2]}" else list[1]
+            loop@ while (n <= book.lastIndex) {
+                val elementInBook = book[n].substringAfter(" ")
 
                 if (element == elementInBook) {
                     counter++
@@ -121,57 +121,15 @@ class PhoneBook {
         )
     }
 
-/*    fun quickSort(leftIndex: Int, rightIndex: Int) {
-        val pivotIndex = (leftIndex + rightIndex) / 2
-        var l = leftIndex
-        var r = rightIndex
-        //val queue = ArrayDeque<String>()
-        if(l >= r) return
-
-        val value = book[pivotIndex].split(" ")
-        val pivot = if (value.size > 2) "${value[1]} ${value[2]}" else value[1]
-        //book.removeAt(pivotIndex)
-        //queue.add(pivot)
-
-        while (true) {
-            var left: String
-            var right: String
-
-            do  {
-                if (l > rightIndex) break
-                val valueLeft = book[l++].split(" ")
-                left = if (valueLeft.size > 2) "${valueLeft[1]} ${valueLeft[2]}" else valueLeft[1]
-            } while (pivot > left)
-
-            do {
-                if (r < leftIndex) break
-                val valueRight = book[r--].split(" ")
-                right = if (valueRight.size > 2) "${valueRight[1]} ${valueRight[2]}" else valueRight[1]
-            } while (pivot < right)
-
-            if (l < r) {
-                book[l] = book[r].also { book[r] = book[l] }
-            } else {
-                break
-            }
-        }
-
-        if (r > leftIndex) quickSort(leftIndex, r)
-        if (l < rightIndex) quickSort(l, rightIndex)
-
-    }*/
-
-
-    /* This function takes last element as pivot, places
-       the pivot element at its correct position in sorted
-       array, and places all smaller (smaller than pivot)
-       to left of pivot and all greater elements to right
-       of pivot */
-    fun partition(low: Int, high: Int): Int {
+    /*     This function takes last element as pivot, places
+           the pivot element at its correct position in sorted
+           array, and places all smaller (smaller than pivot)
+           to left of pivot and all greater elements to right
+           of pivot */
+    private fun partition(low: Int, high: Int): Int {
 
         // pivot
-        val value = book[high].split(" ")
-        val pivot = if (value.size > 2) "${value[1]} ${value[2]}" else value[1]
+        val pivot = book[high].substringAfter(" ")
         // Index of smaller element and
         // indicates the right position
         // of pivot found so far
@@ -180,10 +138,8 @@ class PhoneBook {
 
             // If current element is smaller
             // than the pivot
-            val element = book[j].split(" ")
-            val elementValue = if (element.size > 2) "${element[1]} ${element[2]}" else element[1]
-
-            if (elementValue < pivot) {
+            val element = book[j].substringAfter(" ")
+            if (element < pivot) {
 
                 // Increment index of
                 // smaller element
@@ -196,12 +152,12 @@ class PhoneBook {
         return i + 1
     }
 
-    /* The main function that implements QuickSort
-              arr[] --> Array to be sorted,
-              low --> Starting index,
-              high --> Ending index
-     */
-    fun quickSort(low: Int, high: Int) {
+    /*     The main function that implements QuickSort
+                  arr[] --> Array to be sorted,
+                  low --> Starting index,
+                  high --> Ending index*/
+
+    private fun quickSort(low: Int, high: Int) {
         if (low < high) {
 
             // pi is partitioning index, arr[p]
@@ -214,25 +170,26 @@ class PhoneBook {
             quickSort(pi + 1, high)
         }
     }
-    fun binarySearch() {
-        var counter = 0
-        val start = System.currentTimeMillis()
 
-        loop@for (element in find) {
+    fun binarySearch() {
+        val start = System.currentTimeMillis()
+        quickSort(0, book.lastIndex)
+        val start2 = System.currentTimeMillis()
+        var counter = 0
+
+        loop@ for (element in find) {
             var low = 0
             var high = book.lastIndex
 
 
             while (low != high) {
-                var mid = (low + high) / 2
-                val list = book[mid].split(" ")
-                val elementInBook = if (list.size > 2) "${list[1]} ${list[2]}" else list[1]
+                val mid = (low + high) / 2
+                val elementInBook = book[mid].substringAfter(" ")
 
                 if (element == elementInBook) {
                     counter++
                     continue@loop
-                }
-                if (element > elementInBook) {
+                } else if (element > elementInBook) {
                     low = mid
                 } else {
                     high = mid
@@ -241,9 +198,18 @@ class PhoneBook {
         }
         val end = System.currentTimeMillis()
         val duration = end - start
+        val searchTime = end - start2
+        val sortingTime = duration - searchTime
+
         println(
             "Found $counter / ${find.size} entries. Time taken: " +
                     "${duration / 60000} min. ${(duration % 60000) / 1000} sec. ${duration % 1000} ms."
+        )
+        println(
+            "Sorting time: ${sortingTime / 60000} min. ${(sortingTime % 60000) / 1000} sec. ${sortingTime % 1000} ms."
+        )
+        println(
+            "Searching time: ${searchTime / 60000} min. ${(searchTime % 60000) / 1000} sec. ${searchTime % 1000} ms."
         )
     }
 }
@@ -251,19 +217,14 @@ class PhoneBook {
 fun main() {
     PhoneBook().run {
         println("Start searching (linear search)...")
-        findNumbers()
+        linearSearch()
+
         println()
         println("Start searching (bubble sort + jump search)...")
-        //bubbleSort()
-        //jumpSearch()
-        val start = System.currentTimeMillis()
-        quickSort(0, book.lastIndex)
-        val end = System.currentTimeMillis()
-        val duration = end - start
-        println(
-            "Time taken: " +
-                    "${duration / 60000} min. ${(duration % 60000) / 1000} sec. ${duration % 1000} ms."
-        )
+        bubbleSort()
+
+        println()
+        println("Start searching (quick sort + binary search)...")
         binarySearch()
     }
 }
